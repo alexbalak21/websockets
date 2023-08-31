b69f47e6f826a7394c14e122fa80527da4b6c874
alexbalak21 <alex.balak@outlook.com>
2023-08-31 17:22:59 +0200

    Front Fixed. Server Status fixed. Cant send messages when server disconnected.

--- front/script.js
+++ front/script.js
@@ -6,39 +6,44 @@ const main = document.querySelector("main")
 const input = document.querySelector("input")
 const button = document.querySelector("button")
 
-function connect(adress = "ws://localhost:8765") {
-    let socket = null
-    try {
-        socket = new WebSocket(adress)
-        console.log("Try")
-        socket.addEventListener("open", () => {})
-        server_status.innerText = "Server Connnected"
-        server_status.style.color = "Green"
-        socket.addEventListener("open", () => socket.send("Client Connnected"))
-    } catch (err) {
-        console.log("Server Disconnected")
-    }
-    return socket
-}
+//CREEATINGCONNECTION SOCKET
 
-function Listen_to_client(socket) {
+const SOCKET = new WebSocket("ws://localhost:8765")
+
+function Listen_to_client() {
     input.addEventListener("keypress", (e) => {
+        if (SOCKET.readyState !== 1) {
+            console.log("Server not Connected")
+            return null
+        }
         if (e.key === "Enter" && input.value !== "") {
             const msg = input.value
-            socket.send(msg)
+            SOCKET.send(msg)
             from_client(msg)
             input.value = ""
         }
     })
     button.addEventListener("click", () => {
+        if (SOCKET.readyState !== 1) {
+            console.log("Server not Connected")
+            return null
+        }
         const msg = input.value
         if (input.value !== "") {
-            socket.send(msg)
+            SOCKET.send(msg)
             from_client(msg)
             input.value = ""
         }
     })
-    return socket
+    return null
+}
+
+function Listen_to_server() {
+    if (SOCKET.readyState > 1) {
+        console.log("Server not Connected")
+        return null
+    }
+    SOCKET.onmessage = (event) => from_server(event.data)
 }
 
 function from_client(message) {
@@ -59,32 +64,40 @@ function from_server(message) {
     main.appendChild(p)
 }
 
-function Listen_to_server(socket) {
-    socket.addEventListener("message", (event) => {
-        from_server(event.data)
-    })
+function Disconnect() {
+    SOCKET.close(1000, "Disconnect")
 }
 
-function Disconnect(socket) {
-    socket.close(1000, "Disconnect")
-}
-
-function Listen_to_close(socket) {
+function Listen_to_window_close() {
     window.addEventListener("beforeunload", () => {
-        Disconnect(socket)
+        Disconnect()
     })
+}
+function server_status_checker() {
+    SOCKET.onopen = () => {
+        server_status.innerText = "Server Connected"
+        server_status.style.color = "green"
+    }
+    SOCKET.onerror = () => {
+        server_status.innerText = "Server Error"
+        server_status.style.color = "yellow"
+    }
+    SOCKET.onclose = () => {
+        server_status.innerText = "Server Closed"
+        server_status.style.color = "red"
+    }
 }
 
 function app() {
-    let socket = null
-    socket = connect()
-
-    Listen_to_server(socket)
-    Listen_to_client(socket)
-    Listen_to_close(socket)
+    server_status_checker()
+    Listen_to_server()
+    Listen_to_client()
+    Listen_to_window_close()
 }
 app()
 
 const m = { from: "Alex", to: "Max", message: "Hello" }
 
 console.log(JSON.stringify(m))
+
+console.log(SOCKET.readyState)
